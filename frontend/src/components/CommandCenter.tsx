2b1c600d31de9a05758ed859c681b1786106dfea
Raphael Nussbaumer <nussbaumer@doerler.co.at>
2025-01-09 14:20:13 +0100

    shortcuts

--- frontend/src/components/CommandCenter.tsx
+++ frontend/src/components/CommandCenter.tsx
@@ -1,4 +1,4 @@
-import {Spotlight} from '@mantine/spotlight'
+import {Spotlight, SpotlightActionData} from '@mantine/spotlight'
 import {addNote, exportNotes, openImportDialog} from '../state/notes'
 import {
   logout,
@@ -10,71 +10,88 @@ import {
 } from '../state/user'
 import {selectAnyDialogOpen, useSelector} from '../state/store'
 import {useMantineColorScheme} from '@mantine/core'
+import {useHotkeys} from '@mantine/hooks'
 
 export const CommandCenter = () => {
   const {toggleColorScheme} = useMantineColorScheme()
   const session = useSelector((s) => s.user.user.session)
   const anyDialogOpen = useSelector(selectAnyDialogOpen)
+
+  const commands: (SpotlightActionData & {shortcut?: string})[] = [
+    {
+      id: 'newNote',
+      label: 'New note',
+      onClick: addNote,
+      shortcut: 'alt + n',
+    },
+    {
+      id: 'toggleColorScheme',
+      label: 'Toggle Dark Mode',
+      onClick: toggleColorScheme,
+      shortcut: 'alt + t',
+    },
+    {
+      id: 'sync',
+      label: 'Synchronize notes with server',
+      onClick: openSyncDialogAndSync,
+      disabled: !session,
+      shortcut: 'alt + s',
+    },
+    {
+      id: 'exportNotes',
+      label: 'Export notes',
+      onClick: exportNotes,
+    },
+    {
+      id: 'importNotes',
+      label: 'Import notes',
+      onClick: openImportDialog,
+    },
+    {
+      id: 'register',
+      label: 'Register',
+      onClick: openRegisterDialog,
+      disabled: !!session,
+    },
+    {
+      id: 'login',
+      label: 'Login',
+      onClick: openLoginDialog,
+      disabled: !!session,
+      shortcut: 'alt + l',
+    },
+    {
+      id: 'encryptionKey',
+      label: 'Encryption-Key (Generate/Import/Export)',
+      onClick: openEncryptionKeyDialog,
+    },
+    {
+      id: 'impressum',
+      label: 'Impressum',
+      onClick: toggleImpressum,
+    },
+    {
+      id: 'logout',
+      label: 'Logout',
+      onClick: logout,
+      disabled: !session,
+      shortcut: 'alt + o',
+    },
+  ]
+
+  useHotkeys(
+    commands
+      .filter((c) => !c.disabled)
+      .filter(
+        (c): c is typeof c & {shortcut: string; onClick: () => void} => !!c.shortcut && !!c.onClick
+      )
+      .map((c) => [c.shortcut, c.onClick])
+  )
   return (
     <Spotlight
-      shortcut={['Ctrl + K', 'Cmd + K']}
+      shortcut={['mod + k']}
       disabled={anyDialogOpen}
-      actions={[
-        {
-          id: 'newNote',
-          label: 'New note',
-          onClick: addNote,
-        },
-        {
-          id: 'toggleColorScheme',
-          label: 'Toggle Dark Mode',
-          onClick: toggleColorScheme,
-        },
-        {
-          id: 'sync',
-          label: 'Synchronize notes with server',
-          onClick: openSyncDialogAndSync,
-          disabled: !session,
-        },
-        {
-          id: 'exportNotes',
-          label: 'Export notes',
-          onClick: exportNotes,
-        },
-        {
-          id: 'importNotes',
-          label: 'Import notes',
-          onClick: openImportDialog,
-        },
-        {
-          id: 'register',
-          label: 'Register',
-          onClick: openRegisterDialog,
-          disabled: !!session,
-        },
-        {
-          id: 'login',
-          label: 'Login',
-          onClick: openLoginDialog,
-          disabled: !!session,
-        },
-        {
-          id: 'encryptionKey',
-          label: 'Encryption-Key (Generate/Import/Export)',
-          onClick: openEncryptionKeyDialog,
-        },
-        {
-          id: 'impressum',
-          label: 'Impressum',
-          onClick: toggleImpressum,
-        },
-        {
-          id: 'logout',
-          label: 'Logout',
-          onClick: logout,
-          disabled: !session,
-        },
-      ].filter((a) => !a.disabled)}
+      actions={commands.filter((a) => !a.disabled).map((a) => ({...a, rightSection: a.shortcut}))}
     />
   )
 }
