232fb61e5722c6fade003325a6aa91daa5b07e50
Raphael Nussbaumer <rap.n@icloud.com>
2025-01-14 16:28:22 +0100

    open note full-screen

--- frontend/src/components/OpenNoteDialog.tsx
+++ frontend/src/components/OpenNoteDialog.tsx
@@ -1,31 +1,34 @@
-import {Button, Flex, Modal, Textarea} from '@mantine/core'
+import {Button, Drawer, Flex} from '@mantine/core'
 import {useSelector} from '../state/store'
 import {closeNote, openNoteChanged, deleteOpenNote} from '../state/notes'
 import {modals} from '@mantine/modals'
-import {useRef} from 'react'
 import {IconArrowBackUp} from './icons/IconArrowBackUp'
 import {IconArrowForwardUp} from './icons/IconArrowForwardUp'
 
 export const OpenNoteDialog = () => {
   const note = useSelector((s) => s.notes.openNote)
-  const inputRef = useRef<HTMLTextAreaElement>(null)
   return (
-    <Modal
+    <Drawer
       opened={!!note}
       onClose={closeNote}
-      title='Note'
+      position='bottom'
+      size='100%'
       styles={{
-        content: {flex: 1},
-        body: {display: 'flex', flexDirection: 'column', gap: '1rem'},
+        content: {height: '100dvh', display: 'flex', flexDirection: 'column'},
+        body: {flex: '1 1 0', display: 'flex', flexDirection: 'column', gap: '1rem'},
       }}
     >
-      <Textarea
+      <textarea
         value={note?.txt}
         onChange={(e) => openNoteChanged(e.target.value)}
-        autosize
-        minRows={5}
-        styles={{input: {fontFamily: "Monaco, 'Cascadia Code', Consolas, monospace"}}}
-        ref={inputRef}
+        style={{
+          fontFamily: "Monaco, 'Cascadia Code', Consolas, monospace",
+          resize: 'none',
+          flex: '1 1 0',
+          border: 'none',
+          margin: '4px 0',
+          boxShadow: '0 0 0 1px var(--mantine-color-gray-5)',
+        }}
       />
       <Flex gap='xs'>
         <Button
@@ -49,6 +52,6 @@ export const OpenNoteDialog = () => {
           <IconArrowForwardUp />
         </Button>
       </Flex>
-    </Modal>
+    </Drawer>
   )
 }
