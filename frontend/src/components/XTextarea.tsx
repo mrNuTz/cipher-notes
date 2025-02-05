1d35c8ad9c91a0043f4d9d7b5c8165a1736fe0ad
Raphael Nussbaumer <nussbaumer@doerler.co.at>
2025-02-05 21:46:50 +0100

    use react-simple-code-editor

--- frontend/src/components/XTextarea.tsx
+++ frontend/src/components/XTextarea.tsx
@@ -1,3 +1,5 @@
+import Editor from 'react-simple-code-editor'
+
 export type XTextareaProps = {
   value: string
   onChange: (value: string) => void
@@ -7,51 +9,41 @@ export type XTextareaProps = {
 }
 export const XTextarea = ({value, onChange, onUndo, onRedo, onUp}: XTextareaProps) => {
   return (
-    <textarea
+    <Editor
+      className='x-textarea'
+      tabSize={2}
+      insertSpaces={true}
       value={value}
-      onChange={(e) => onChange(e.target.value)}
+      onValueChange={onChange}
+      highlight={(value) => <>{value}</>}
       onKeyDown={(e) => {
         if (
+          e.currentTarget instanceof HTMLTextAreaElement &&
           (e.key === 'Backspace' || e.key === 'ArrowUp') &&
           e.currentTarget.selectionStart === 0
         ) {
           e.preventDefault()
           onUp()
-        }
-        if (e.key === 'Tab' && !e.shiftKey && !e.ctrlKey) {
-          e.preventDefault()
-          const target = e.currentTarget
-          const cursor = target.selectionStart
-          const value = target.value
-          onChange(value.slice(0, cursor) + '\t' + value.slice(cursor))
-          Promise.resolve().then(() => {
-            target.selectionStart = cursor + 1
-            target.selectionEnd = cursor + 1
-          })
-        }
-
-        if ((e.metaKey || e.ctrlKey) && e.key.toLowerCase() === 'z') {
+        } else if ((e.metaKey || e.ctrlKey) && e.key.toLowerCase() === 'z') {
           e.preventDefault()
           if (e.shiftKey) {
             onRedo()
           } else {
             onUndo()
           }
-        }
-        if ((e.metaKey || e.ctrlKey) && e.key.toLowerCase() === 'y') {
+        } else if ((e.metaKey || e.ctrlKey) && e.key.toLowerCase() === 'y') {
           e.preventDefault()
           onRedo()
         }
       }}
       style={{
         fontFamily: "Monaco, 'Cascadia Code', Consolas, monospace",
-        resize: 'none',
         flex: '1 1 0',
         border: 'none',
         outline: 'none',
         backgroundColor: 'transparent',
         margin: '4px 0',
-        tabSize: 4,
+        tabSize: 2,
       }}
     />
   )
