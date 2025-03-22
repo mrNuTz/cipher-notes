6352a1d8025c29915b624d077fbebb9aca281ede
Raphael Nussbaumer <nussbaumer@doerler.co.at>
2025-03-22 12:48:17 +0100

    proxy setup without cors; https setup; update example.env

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -8,11 +8,7 @@ export const config = createConfig({
   http: {
     listen: env.PORT,
   },
-  cors: ({defaultHeaders}) => ({
-    ...defaultHeaders,
-    'Access-Control-Allow-Origin': env.ACCESS_CONTROL_ALLOW_ORIGIN,
-    'Access-Control-Allow-Credentials': 'true',
-  }),
+  cors: false,
   jsonParser: express.json({limit: env.LIMIT_JSON}),
   rawParser: express.raw({limit: env.LIMIT_RAW}),
   upload: false,
